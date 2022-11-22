171cd2a854fd18125f04ecf3aa6b277e72dd5450
YESSENIA PAOLA CARBAJAL ARMENTA <yessenia.carbajal2864@alumno.udg.mx>
2022-11-21 18:00:42 -0600

    Agregar archivos de proyecto.

---
+++ OpenShift/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace OpenShift
+{
+    partial class OpenShift
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(148, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 78);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SHOW";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("OCR A Extended", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(-9, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(487, 256);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tarea con OpenShift";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // OpenShift
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(474, 482);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "OpenShift";
+            this.Text = "OpenShift";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+    }
+}
---
+++ OpenShift/Form1.cs
@@ -0,0 +1,20 @@
+namespace OpenShift
+{
+    public partial class OpenShift : Form
+    {
+        public OpenShift()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.Black;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
